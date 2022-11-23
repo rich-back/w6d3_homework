e2bcf45e3c709d6378a486d81c7483f24d9de863
Richard Backhouse <richard@Richards-MacBook-Pro-2.local>
2022-11-23 21:27:00 +0000

    completed homework

---
+++ models/traveller.js
@@ -0,0 +1,53 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance > minDistance;
+  })
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((runningTotal, journey) => {
+    return runningTotal + journey.distance;
+  } ,0 );
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  let results = [];
+  this.journeys.forEach((journey) => {
+    results.push(journey.transport);
+  })
+  let uniqueTrasnport =[...new Set(results)];
+  return uniqueTrasnport;
+};
+
+
+
+
+
+
+
+
+
+module.exports = Traveller;
